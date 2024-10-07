3babc2a7209de8872ded478cd8ddee4d16b61d3a
Dhireen Kumar Rajak <iamdeerajak@gmail.com>
2024-10-07 16:42:33 +0530

    Second Commit

---
+++ WebMVCEx01/WebMVCEx01/Models/Emp.cs
@@ -0,0 +1,10 @@
+﻿namespace WebMVCEx01.Models
+{
+    public class Emp
+    {
+        public int EId { get; set; }
+        public string EName { get; set; }
+        public string EDesignation { get; set; }
+        public DateTime EDOJ { get; set; }
+    }
+}
